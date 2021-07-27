456b089743865234b0114a57242d59e910cb2b1b
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-07-27 12:17:51 +0530

    Add files via upload

---
+++ if else2.py
@@ -0,0 +1,20 @@
+# cost=int(input("enter the number"))
+# amount=int(input("enter the number"))
+# if cost+amount:
+#     print("total profit",amount)
+# elif cost-amount:
+#     print("total loss",amount)
+# else:
+#     print("no profit not loss")
+
+x=int(input("enter the number"))
+var=x/1000*100
+if x>100:
+    print("shopping greater than 1000",var)
+else:
+    print("shopping less on so discount")
+
+
+
+
+
---
+++ ifelse1.py
@@ -0,0 +1,43 @@
+# alphabet=input("enter the alphabet")
+# if alphabet=="A":
+#     print("uppercase alphabet")
+# else:
+#     print("lowercase alphabet")
+
+
+# year=int(input("enter the years"))
+# if year%4==0:
+#     print("leaf years")
+# else:
+#     print("not leaf years")
+
+
+# number=int(input("enter the number"))
+# if number%5==0 and number%11!=0:
+#     print("divisible",number)
+# else:
+#     print("not divisible",number)
+
+
+# a=int(input("enter the number "))
+# b=int(input("enter the number "))
+# if a>=b:
+#     print(a,"greater ")
+# else:
+#     print("greater")
+
+# num1=int(input("enter the number :-"))
+# num2=int(input("enter the number :-"))
+# num3=int(input("enter the number :-"))
+# if num1>num2 and num1>num3:
+#     print("num1",num1)
+# elif num2>num1 and num2>num3:
+#     print("num2",num2)
+# else:
+#     print("num3",num3)
+
+chr=input("enter the character")
+if (chr>="a" and chr<="z") or (chr>="A" and chr<="Z"):
+    print("it is alphabet")
+else:
+    print("it is not alphabet")
