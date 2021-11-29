99b8d6511995a469905a744ce388b5ae3ee8a799
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-11-29 15:47:31 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,61 @@
+# num=list(input("enter the number"))
+# a=len(num)-2
+# if num[a]=='3':
+#     print("yes")
+# else:
+#     print("no")
+    
+
+# a={"p":1,"b":2}
+# if len(a)==0:
+#     print("empty dictinary")
+# else:
+#     print("not empty dictinary")
+
+    
+# i=1
+# while i<8:
+#     j=1
+#     while j<=8-i:
+#         print(chr(64+i),end=" ")
+#         j=j+1
+#     print( )   
+#     i=i+1
+
+# i=1
+# while i<5:
+#     j=1
+#     while j<=5-i:
+#         print("*",end=" ")
+#         j=j+1
+#     print( )
+#     i=i+1
+
+
+# row=0
+# while row<6:
+#     column=0
+#     while column<7:
+#         if (row==0 and column%3!=0)or(row==1 and column%3==0)or(row-column==2)or(row+column==8):
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#         column=column+1
+#     print( )
+#     row=row+1
+
+# num=int(input("enter the number"))
+# i=1
+# while i<=10:
+#     print(num,"*",i,"=",num*i)
+#     i=i+1
+
+# num=input("enter the character")
+# a="aeiouAEIOU"
+# if num in a:
+#     print("vowels")
+# else:
+#     print("consanant")
+
+
+
